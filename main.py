6f9e846a2cc6ed19f1200bc2fd19056bbf143b0c
Joey Willhite <joeycw328@gmail.com>
2014-11-15 20:27:22 -0800

    compare cp header with sent header

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ class couchpotato(webapp2.RequestHandler):
 				cpcon.connect()
 				apicall=re.findall('/api.*',self.request.path).pop()
 				cpcon.request("GET", apicall)
-				self.response.write(cpcon.getresponse().getheaders())
+				self.response.write("CP header:" +cpcon.getresponse().getheaders()+'\nResponse header:'+self.response.headerlist)
 				cpcon.close()
 			except NeedIndexError:
 				self.response.write("No recent IP Address Updates")
